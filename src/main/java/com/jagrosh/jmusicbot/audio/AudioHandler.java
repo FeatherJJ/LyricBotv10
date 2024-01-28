a44c17a3f95e09527c53863d5573713a10e7a302
DavidIsAGorilla <113325455+DavidIsAGorilla@users.noreply.github.com>
2024-01-28 13:40:46 -0800

    dfsdf
    fsfdssdf

--- src/main/java/com/jagrosh/jmusicbot/audio/AudioHandler.java
+++ src/main/java/com/jagrosh/jmusicbot/audio/AudioHandler.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.jagrosh.jmusicbot.audio;
 
 import com.jagrosh.jmusicbot.JMusicBot;
@@ -40,10 +25,6 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 
-/**
- *
- * @author John Grosh <john.a.grosh@gmail.com>
- */
 public class AudioHandler extends AudioEventAdapter implements AudioSendHandler 
 {
     public final static String PLAY_EMOJI  = "\u25B6"; // ▶
